bf4cbd3f06aa4f9be410d83b7050855aff2962ab
Thieen226 <t.ho1@my.metroed.net>
2024-04-11 08:32:31 -0700

    add

---
+++ quiz/quiz/quizApp.swift
@@ -0,0 +1,17 @@
+//
+//  quizApp.swift
+//  quiz
+//
+//  Created by StudentAM on 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct quizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
